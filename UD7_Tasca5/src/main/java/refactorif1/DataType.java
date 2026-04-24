ed71901bf6e5385ad081464b532780db60b12c99
Neivajg <jimenez.gonzalez.neiva@gmail.com>
2026-04-24 21:07:14 +0200

    extraer constantes DATE_CODES y NUMERIC_CODES

--- UD7_Tasca5/src/main/java/refactorif1/DataType.java
+++ UD7_Tasca5/src/main/java/refactorif1/DataType.java
@@ -1,6 +1,18 @@
 package main.java.refactorif1;
 
+import java.util.Set;
+
 public class DataType {
+
+    private static final Set<Integer> DATE_CODES = Set.of(
+            702, 1082, 1083, 1114, 1184, 1266, 12403
+    );
+
+    private static final Set<Integer> NUMERIC_CODES = Set.of(
+            20, 21, 23, 24, 26, 700, 701, 790,
+            1700, 2202, 2203, 2204, 2205, 2206,
+            3734, 3769, 12396
+    );
 
     public static void main(String[] args) {
         System.out.println(DataType.gd(20));
