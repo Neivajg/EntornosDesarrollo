6d915f002c1ffe1574b2650081b250f892887f29
Neivajg <jimenez.gonzalez.neiva@gmail.com>
2026-04-24 20:08:01 +0200

    refactorización suma digitos

--- UD7_Tasca2/src/main/java/divisible3/Divisible3.java
+++ UD7_Tasca2/src/main/java/divisible3/Divisible3.java
@@ -7,13 +7,18 @@ public class Divisible3 {
         int suma = 0;
 
         for (int i = 1; i <= n; i++) {
-            String num = String.valueOf(i);
+            suma += sumaDigitos(i);
+        }
+        return suma % 3 == 0 ? "SI" : "NO";
+    }
+    private static int sumaDigitos(int numero) {
+        int suma = 0;
 
-            for (char c : num.toCharArray()) {
-                suma += c - '0';
-            }
+        while (numero > 0) {
+            suma += numero % 10;
+            numero /= 10;
         }
 
-        return (suma % 3 == 0) ? "SI" : "NO";
+        return suma;
     }
 }
