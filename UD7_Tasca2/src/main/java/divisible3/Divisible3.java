21a7bdd20b679348a54fd52a968894676ecf7de8
Neivajg <jimenez.gonzalez.neiva@gmail.com>
2026-04-24 20:00:01 +0200

    4 commit,implementacion basica

--- UD7_Tasca2/src/main/java/divisible3/Divisible3.java
+++ UD7_Tasca2/src/main/java/divisible3/Divisible3.java
@@ -3,6 +3,17 @@ package main.java.divisible3;
 public class Divisible3 {
 
     public static String esDivisible(int n) {
-        return "SI"; // hardcode para pasar primer test
+
+        int suma = 0;
+
+        for (int i = 1; i <= n; i++) {
+            String num = String.valueOf(i);
+
+            for (char c : num.toCharArray()) {
+                suma += c - '0';
+            }
+        }
+
+        return (suma % 3 == 0) ? "SI" : "NO";
     }
 }
