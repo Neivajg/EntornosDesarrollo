81d54cc3f1398fcc071c2a12b87a47f857789bc5
Neivajg <jimenez.gonzalez.neiva@gmail.com>
2026-04-25 18:56:24 +0200

    Crear test fly

--- UD7_Tasca7/src/test/java/songtdd/TDDSongTest.java
+++ UD7_Tasca7/src/test/java/songtdd/TDDSongTest.java
@@ -5,8 +5,18 @@ import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 
 public class TDDSongTest {
+
     @Test
     public void listaVacia() {
         assertEquals("", TDDSong.song(new String[]{}));
+    }
+
+    @Test
+    public void fly() {
+        assertEquals(
+                "There was an old lady who swallowed a fly.\n" +
+                        "I don't know why she swallowed a fly - perhaps she'll die!\n",
+                TDDSong.song(new String[]{"fly"})
+        );
     }
 }
