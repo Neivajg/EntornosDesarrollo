bbf76824cf35ec57264434b80f3ec07e2ca3e36e
Neivajg <jimenez.gonzalez.neiva@gmail.com>
2026-04-25 18:55:31 +0200

    Crear primer test lista vacia

--- UD7_Tasca7/src/main/java/songtdd/TDDSong.java
+++ UD7_Tasca7/src/main/java/songtdd/TDDSong.java
@@ -2,4 +2,7 @@ package main.java.songtdd;
 
 public class TDDSong {
 
+    public static String song(String[] strings) {
+        return "";
+    }
 }
--- UD7_Tasca7/src/test/java/songtdd/TDDSongTest.java
+++ UD7_Tasca7/src/test/java/songtdd/TDDSongTest.java
@@ -1,5 +1,12 @@
 package test.java.songtdd;
 
+import main.java.songtdd.TDDSong;
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
 public class TDDSongTest {
-    
+    @Test
+    public void listaVacia() {
+        assertEquals("", TDDSong.song(new String[]{}));
+    }
 }
